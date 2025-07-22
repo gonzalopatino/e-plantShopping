d955f1af69045534bc09b64aa7a3c1664d998dd1
Gonzalo <gpatinoc92@gmail.com>
2025-07-22 00:42:53 -0700

    final version for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/shoppingreact/paradise-nursery-shopping-cart-app/',
+  base: '/Learning React/',
   plugins: [react()],
 });
