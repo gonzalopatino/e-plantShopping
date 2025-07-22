36e6967b56f850dc8ad225af3a97754f4516d327
Gonzalo <gpatinoc92@gmail.com>
2025-07-22 00:31:22 -0700

    final version

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -278,12 +278,13 @@ function ProductList() {
                 <div className="tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <a href="/paradise-nursery-shopping-cart-app/" style={{ textDecoration: 'none' }}>
-                            <div>
-                                <h3 style={{ color: 'white' }}>Milan's Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
-                            </div>
-                        </a>
+                        <div onClick={handleContinueShopping} style={{ textDecoration: 'none', cursor: 'pointer' }}>
+                        <div>
+                            <h3 style={{ color: 'white' }}>Milan's Nursery</h3>
+                            <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
+                        </div>
+                        </div>
+
                     </div>
                 </div>
                 <div style={styleObjUl}>
